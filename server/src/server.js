56c5b8270ca96b91568ab08313f9151cd132b589
pppacific <hohohorlick259@gmail.com>
2026-06-23 11:57:37 +0800

    server add get api

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { dogs } from "./db/schema.js";
+import {desc} from "drizzle-orm";
 // import rateLimiter from "./middleware/rateLimiter.js";
 
 // import job from "./config/cron.js";
@@ -57,6 +58,38 @@ app.post("/api/dog", async (req, res) => {
     res.status(500).json({ error: "Something went wrong" });
   }
 });
+app.get("/api/dogs", async (req, res) => {
+    try {
+
+        const allDogs = await db.select().from(dogs).orderBy(desc(dogs.createdAt));
+
+        res.status(200).json({
+            success: true,
+            data: allDogs,
+        });
+
+    } catch (error) {
+        console.log("Error fetching the dogs", error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+});
+// app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+//     try {
+//         const { userId, recipeId } = req.params;
+//
+//         await db
+//             .delete(favoritesTable)
+//             .where(
+//                 and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, parseInt(recipeId)))
+//             );
+//
+//         res.status(200).json({ message: "Favorite removed successfully" });
+//     } catch (error) {
+//         console.log("Error removing a favorite", error);
+//         res.status(500).json({ error: "Something went wrong" });
+//     }
+// });
+
 // initDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is up and running on PORT:", PORT)
