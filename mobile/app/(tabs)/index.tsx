dbac5d715e1002f683aefb9ce3a957d1e61e2325
pppacific <hohohorlick259@gmail.com>
2026-07-09 01:19:02 +0800

    add avatar

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,9 +1,8 @@
-import { Text, View } from "react-native";
+import { Image,Text, View } from "react-native";
 import "@/global.css"
-import {Link} from "expo-router";
 import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 import {styled} from "nativewind";
-
+import images from "@/constants/images"
 const SafeAreaView = styled(RNSafeAreaView)//enable classname support
 export default function Index() {
   return (
@@ -11,16 +10,25 @@ export default function Index() {
           <Text className="text-5xl font-sans-extrabold text-blue-500">
               Welcome Home!
           </Text>
-          <Link href={'/'} className={`mt-4 font-sans-bold rounded bg-primary text-white p-4`}>to home</Link>
-          <Link href={'/(auth)/sign-in'} className={`mt-4 font-sans-bold rounded bg-primary text-white p-4`}>to sign in</Link>
-          <Link href={'/(auth)/sign-up'} className={`mt-4 font-sans-bold rounded bg-primary text-white p-4`}>to sign up</Link>
+          <View className="home-header">
+              <View className="home-user">
+                  <Image
+                      source={images.avatar}
+                      className="home-avatar"
+                  />
+                  {/*<Text className="home-user-name">{displayName}</Text>*/}
+              </View>
+          </View>
+          {/*<Link href={'/'} className={`mt-4 font-sans-bold rounded bg-primary text-white p-4`}>to home</Link>*/}
+          {/*<Link href={'/(auth)/sign-in'} className={`mt-4 font-sans-bold rounded bg-primary text-white p-4`}>to sign in</Link>*/}
+          {/*<Link href={'/(auth)/sign-up'} className={`mt-4 font-sans-bold rounded bg-primary text-white p-4`}>to sign up</Link>*/}
 
-          <Link href={'/doggie/Karin'} className={`mt-4 rounded bg-primary text-white p-4`}>Karin</Link>
-          <Link href={{
-              pathname: '/doggie/[id]',
-              params: {id:"karin"}
-          }}
-                className={`mt-4 rounded bg-primary text-white p-4`}>see doggie</Link>
+          {/*<Link href={'/doggie/Karin'} className={`mt-4 rounded bg-primary text-white p-4`}>Karin</Link>*/}
+          {/*<Link href={{*/}
+          {/*    pathname: '/doggie/[id]',*/}
+          {/*    params: {id:"karin"}*/}
+          {/*}}*/}
+          {/*      className={`mt-4 rounded bg-primary text-white p-4`}>see doggie</Link>*/}
 
       </SafeAreaView>
   );
